c3e34c424a4ea43c2eed387f5eac63fbcef02c7c
Gama Pradipta Wirawan <gamapradipta88@gmail.com>
2020-03-13 08:46:51 +0700

    Setup room Database

--- app/src/main/java/com/example/bolasepak/database/DatabaseBolaSepak.java
+++ app/src/main/java/com/example/bolasepak/database/DatabaseBolaSepak.java
@@ -1,6 +1,12 @@
 package com.example.bolasepak.database;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.example.bolasepak.model.*;
+
+@Database(entities = {Event.class, Team.class},version = 1,exportSchema = false)
 public abstract class DatabaseBolaSepak extends RoomDatabase {
+    public abstract EventDao eventDao();
+    public abstract TeamDao teamDao();
 }
--- app/src/main/java/com/example/bolasepak/database/TeamDao.java
+++ app/src/main/java/com/example/bolasepak/database/TeamDao.java
@@ -1,4 +1,23 @@
 package com.example.bolasepak.database;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.bolasepak.model.Team;
+
+import java.util.List;
+
+@Dao
 public interface TeamDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void addTeam(Team Team);
+
+    @Query("SELECT * FROM team WHERE id_team LIKE :keyword")
+    public List<Team> searchTeamById(String keyword);
+
+    @Update
+    public void updateTeam(Team team);
 }
