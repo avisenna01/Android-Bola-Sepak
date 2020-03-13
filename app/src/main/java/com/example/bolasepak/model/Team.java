c422c562e255c86b5e7756cb6ce2e9a5ea19058c
Gama Pradipta Wirawan <gamapradipta88@gmail.com>
2020-03-13 08:46:02 +0700

    Modify Event and Team model

--- app/src/main/java/com/example/bolasepak/model/Team.java
+++ app/src/main/java/com/example/bolasepak/model/Team.java
@@ -1,4 +1,60 @@
 package com.example.bolasepak.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "team")
 public class Team {
+    @PrimaryKey
+    @ColumnInfo(name = "id_team")
+    @NonNull
+    private String id_team;
+    @ColumnInfo(name = "name")
+    private String name;
+    @ColumnInfo(name = "urlPict")
+    private String urlPict;
+    @ColumnInfo(name = "description")
+    private String description;
+
+    public Team(@NonNull String id_team, String name, String urlPict, String description) {
+        this.id_team = id_team;
+        this.name = name;
+        this.urlPict = urlPict;
+        this.description = description;
+    }
+
+    @NonNull
+    public String getId_team() {
+        return id_team;
+    }
+
+    public void setId_team(@NonNull String id_team) {
+        this.id_team = id_team;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrlPict() {
+        return urlPict;
+    }
+
+    public void setUrlPict(String urlPict) {
+        this.urlPict = urlPict;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
